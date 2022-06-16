51a87d5d938d5d58855f042c7483b8805719564e
rnce414 <99392812+rnce414@users.noreply.github.com>
2022-06-16 15:05:17 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='header-el'>
-        <img src="resume-bg.png" className='img-el' />
+        <img src="pic.png" className='img-el' />
 
     </div>
    
